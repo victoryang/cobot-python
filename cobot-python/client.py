48b8eec5f883fde5c83176c260f6231e831a6006
Tao Yang <yangtao3601@126.com>
2019-02-13 18:17:37 +0800

    object client

---
+++ cobot-python/client.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import http
+import json
+import base64
+
+DefaultPort = ":9000"
+
+class Client(Object):
+    token = ""
+
+    def __init__(self, addr):
+        self.__addr = ipaddr
+        self.__conn = http.client.HTTPSConnection(addr + DefaultPort)
+
+    @property
+    def addr(self):
+        return self.__addr
+
+    def register(self, username, password):
+        url = self.__addr + "/v1/login?username=" + username + "&pwd=" + base64.urlsafe_b64encode(password)
+        self.request("POST", url)
+
+    def request(self, method, path, data):
+        url = self.__addr + path
+
+        if data is not None:
+            data = json.dump(data)
+
+        headers = {}
+        headers["Content-Type"] = "application/json"
+        headers["Accept"] = "application/json"
+        if self.token != "":
+            headers["Authorization"] = "bearer " + self.token
+
+        self.__conn.request(method, url, data, headers)
+        return self.__conn.getresponse()
