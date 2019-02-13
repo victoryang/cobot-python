d8c85e40e4e0d89c884c22dd3a0d2297074c224c
victoryang <victoryang@github.com>
2019-02-13 15:12:39 +0800

    init

---
+++ README.rst
@@ -0,0 +1,14 @@
+# The python version for cobot
+
+## Download
+    git clone https://github.com/victoryang/cobot-python
+
+## Prequisite
+    pip
+
+## Setup tools
+    pip install setuptools
+    python setup.py sdist
+
+## Install
+    pip install ./dist/cobot-python-0.1.tar.gz
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cobot-python',
+    version='0.1',
+    description='The sdk for cobot',
+    license='MIT',
+    author='victoryang',
+    author_email='yangtao3601@126.com',
+    url='https://github.com/victoryang/cobot-python'
+)
