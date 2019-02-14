488a0e90527773b245088bf95b843f0bd55fd099
victoryang <victoryang@github.com>
2019-02-14 14:04:27 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='cobot_python',
+    name='cobot-python',
     version='0.1',
     description='The sdk for cobot',
     license='MIT',
