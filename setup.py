ec512f406f16adc0e4a9c18ace65a3909025dd7d
victoryang <victoryang@github.com>
2019-02-14 14:01:33 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='cobot_python',
@@ -7,5 +7,6 @@ setup(
     license='MIT',
     author='victoryang',
     author_email='yangtao3601@126.com',
+    packages=find_packages(),
     url='https://github.com/victoryang/cobot-python'
 )
