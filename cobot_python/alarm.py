aa8600e00e6e9e3628d548faf8689e9ac7b4a4ff
Tao Yang <yangtao3601@126.com>
2019-02-28 18:00:52 +0800

    fix

---
+++ cobot_python/alarm.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import context
+
+def reset(ctx):
+    """Reset alarm info
+    Args:
+        ctx: Context
+
+    Returns:
+        Success: True
+        Failure: False
+    """
+    r = ctx.tran.post("/v2/alarmservice/robot/reset")
+    if r[0] != 200:
+        return False
+
+    return r[1]
+
+def get_latest_alarms(ctx):
+    """Get latest alarm info
+    Args:
+        ctx: Context
+
+    Returns:
+        Success: dict: {
+            "time": 1551255491,
+            "err_level": 0,
+            "err_no": 10001,
+            "sub_err_no": 0,
+            "message": "get arc weld data error"
+
+        }
+        Failure: False
+    """
+    r = ctx.tran.get("/v2/alarmservice/alarms/latest")
+    if r[0] != 200:
+        return False
+
+    ret = r[1]
+    return {
+        "time": ret["time"],
+        "err_level": ret["errLevel"],
+        "err_no": ret["errNo"],
+        "sub_err_no": ret["subErrNo"],
+        "message": ret["message"]
+    }
--- test.py
+++ test.py
@@ -4,4 +4,17 @@ ctx = context.Context("192.168.1.253", 9000)
 
 ctx.login("Admin", "333333")
 
+print ctx.tran.token
+
+print "check health: " + str(ctx.check_health())
+
+state = param.get_robot_state(ctx)
+print type(state)
+
+print "robot state: " + str(state)
+
+print "power on: " + str(servo.power(ctx))
+
+print "get input status: " + str(io_service.get_input(ctx, 0))
+
 ctx.logout()
