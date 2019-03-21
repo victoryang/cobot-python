300be27cba35f61c4e943167dbfebdd028e1e6fd
Tao Yang <yangtao3601@126.com>
2019-03-21 18:00:33 +0800

    code refactor

--- cobot_python/movement.py
+++ cobot_python/movement.py
@@ -1,329 +1,197 @@
 #!/usr/bin/env python
 
+# -*- coding: utf-8 -*-
+"""
+
+This module contains the interfaces of Movement Service
+
+"""
+
 import context
-import schedule
 import param
 
-from threading import Thread
-
-def robot_state_stop(ctx):
-    state = param.get_robot_state(ctx)
-    if state == param.ROBOT_STATE_STOP or state == param.ROBOT_STATE_ERROR:
-        return True
-
-    return False
-
-def do_movement_check(ctx, target_pos):
-    sch = schedule.Scheduler(robot_state_stop, 60)
-
-    if sch.do(ctx):
-        pos = get_robot_pos(ctx)
-        if set(pos) == set(target_pos):
-            return 0
-        else:
-            if param.get_robot_state(ctx) == param.ROBOT_STATE_ERROR:
-                return -2
-            else:
-                return -1
-
-    return -3
-
-def cb_example(ret):
-    print "ret is " + str(ret)
-
-def do_movement_check_async(ctx, target_pos, cb=cb_example):
-    ret = do_movement_check(ctx, target_pos)
-    cb(ret)
-
 def joint_move(ctx, target_pos, speed):
-    """Run joint move synchronously
+    """关节运动
+
     Args:
-        ctx: Context
-        target_pos: list: target position, [0,90,0,0,0,0,0,0]
-        speed: float: Running speed, 0.5
-
-    Returns:
-        Success: 0
-        Failure: Other
-    """
-    data = {
-        "targetPos": target_pos,
-        "speed": speed
-    }
-    r = ctx.tran.post("/v2/movementservice/robot/movement/joint", data)
-    if r[0] != 200:
-        return False
-
-    return do_movement_check(ctx, target_pos)
-
-def joint_move_async(ctx, target_pos, speed, cb=cb_example):
-    """Run joint move asynchronously
-    Args:
-        ctx: Context
-        target_pos: list: target position, [0,90,0,0,0,0,0,0]
-        speed: float: Running speed, 0.5
-        cb: function: callback function, default cb_example
+        ctx (context.Context): 登陆上下文
+        target_pos (list): 目标关节点, [0,90,0,0,0,0,0,0]
+        speed (float): 运行速度百分比，范围:[1~100]，单位：百分比
 
     Returns:
         Success: True
         Failure: False
     """
+
     data = {
         "targetPos": target_pos,
         "speed": speed
     }
 
-    r = ctx.tran.post("/v2/movementservice/robot/movement/joint", data)
-    if r[0] != 200:
-        return False
-
-    Thread(target=do_movement_check_async, args=(ctx, target_pos, cb)).start()
-
-    return r[1]
+    return ctx.tran.post("/v2/movementservice/robot/movement/joint", data)["success"]
 
 def line_move(ctx, target_pos, speed):
-    """Run line move synchronously
+    """直线运动
+
     Args:
-        ctx: Context
-        target_pos: list: target position, [0,90,0,0,0,0,0,0]
-        speed: int: Running speed, [1-3000]
-
-    Returns:
-        Success: 0
-        Failure: Other
-    """
-    data = {
-        "targetPos": target_pos,
-        "speed": speed
-    }
-    r = ctx.tran.post("/v2/movementservice/robot/movement/line", data)
-    if r[0] != 200:
-        return False
-
-    return do_move_check(ctx, target_pos)
-
-def line_move_async(ctx, target_pos, speed, cb=cb_example):
-    """Run line move asynchronously
-    Args:
-        ctx: Context
-        target_pos: list: target position, [0,90,0,0,0,0,0,0]
-        speed: float: Running speed, 0.5
-        cb: function: callback function, default cb_example
+        ctx (context.Context): 登陆上下文
+        target_pos (list): 目标点, [0,90,0,0,0,0,0,0]
+        speed (float): 运行速度，范围[1-3000]，单位：毫米/秒
 
     Returns:
         Success: True
         Failure: False
     """
+
     data = {
         "targetPos": target_pos,
         "speed": speed
     }
 
-    r = ctx.tran.post("/v2/movementservice/robot/movement/line", data)
-    if r[0] != 200:
-        return False
-
-    Thread(target=do_movement_check_async, args=(ctx, target_pos, cb)).start()
-
-    return r[1]
+    return ctx.tran.post("/v2/movementservice/robot/movement/line", data)["success"]
 
 def arc_move(ctx, mid_pos, target_pos, speed):
-    """Run arc move synchronously
+    """圆弧运动, 以机器人当前点作为第一个点，另外给定的两个点构成一个完成圆弧
+
     Args:
-        ctx: Context
-        mid_pos: list: second target position
-        target_pos: list: target position, [0,90,0,0,0,0,0,0]
-        speed: int: Running speed, [1-3000]
-
-    Returns:
-        Success: 0
-        Failure: Other
-    """
-    data = {
-        "midPos": mid_pos,
-        "targetPos": target_pos,
-        "speed": speed
-    }
-    r = ctx.tran.post("/v2/movementservice/robot/movement/arc", data)
-    if r[0] != 200:
-        return False
-
-    return do_move_check(ctx, target_pos)
-
-def arc_move_async(ctx, mid_pos, target_pos, speed, cb=cb_example):
-    """Run arc move asynchronously
-    Args:
-        ctx: Context
-        mid_pos: list: second target position 
-        target_pos: list: target position, [0,90,0,0,0,0,0,0]
-        speed: int: Running speed, [1-3000]
-        cb: function: callback function, default cb_example
+        ctx (context.Context): 登陆上下文
+        mid_pos (list): 第二目标点（相对与当前机器人位置）
+        target_pos (list): 目标点, [0,90,0,0,0,0,0,0]
+        speed (float): 运行速度，范围[1-3000]，单位：毫米/秒
 
     Returns:
         Success: True
         Failure: False
     """
+
     data = {
         "midPos": mid_pos,
         "targetPos": target_pos,
         "speed": speed
     }
 
-    r = ctx.tran.post("/v2/movementservice/robot/movement/arc", data)
-    if r[0] != 200:
-        return False
-
-    Thread(target=do_movement_check_async, args=(ctx, target_pos, cb)).start()
-
-    return r[1]
+    return ctx.tran.post("/v2/movementservice/robot/movement/arc", data)["success"]
 
 def rotate_move(ctx, target_pos, speed):
-    """Run rotate move synchronously
+    """旋转运动
+
     Args:
-        ctx: Context
-        target_pos: list: target position, [0,90,0,0,0,0,0,0]
-        speed: int: Running speed
-
-    Returns:
-        Success: 0
-        Failure: Other
-    """
-    data = {
-        "targetPos": target_pos,
-        "speed": speed
-    }
-    r = ctx.tran.post("/v2/movementservice/robot/movement/rotate", data)
-    if r[0] != 200:
-        return False
-
-    return do_move_check(ctx, target_pos)
-
-def rotate_move_async(ctx, target_pos, speed, cb=cb_example):
-    """Run rotate move asynchronously
-    Args:
-        ctx: Context
-        target_pos: list: target position, [0,90,0,0,0,0,0,0]
-        speed: float: Running speed
-        cb: function: callback function, default cb_example
+        ctx (context.Context): 登陆上下文
+        target_pos (list): 目标点, [0,90,0,0,0,0,0,0]
+        speed (float): 运行速度，单位: 度/秒
 
     Returns:
         Success: True
         Failure: False
     """
+
     data = {
         "targetPos": target_pos,
         "speed": speed
     }
 
-    r = ctx.tran.post("/v2/movementservice/robot/movement/line", data)
-    if r[0] != 200:
-        return False
-
-    Thread(target=do_movement_check_async, args=(ctx, target_pos, cb)).start()
-
-    return r[1]
+    return ctx.tran.post("/v2/movementservice/robot/movement/arc", data)["success"]
 
 def set_waypoint_max_joint_speed(ctx, speed):
-    """Set max joint speed
+    """设置多路点运动时最大关节速度
+
     Args:
-        ctx: Context
-        speed: float: Max joint speed
+        ctx (context.Context): 登陆上下文
+        speed (float): 关节速度，范围:[%1~100%], 单位：百分比
 
     Returns:
         Success: True
         Failure: False
     """
-    r = ctx.tran.put("/v2/movementservice/robot/movement/waypoints/maxjoinspeed/"+ str(speed))
-    if r[0] != 200:
-        return False
 
-    return r[1]
+    return ctx.tran.put("/v2/movementservice/robot/movement/waypoints/maxjoinspeed/"+ str(speed))["success"]
 
 def set_waypoint_max_line_speed(ctx, speed):
-    """Set max line speed
+    """设置多路点运动时最大直线速度
+
     Args:
-        ctx: Context
-        speed: float: Max line speed
+        ctx (context.Context): 登陆上下文
+        speed (float): 直线速度，范围:[1~3000], 单位：毫米/秒
 
     Returns:
         Success: True
         Failure: False
     """
-    r = ctx.tran.put("/v2/movementservice/robot/movement/waypoints/maxlinespeed/"+ str(speed))
-    if r[0] != 200:
-        return False
 
-    return r[1]
+    return ctx.tran.put("/v2/movementservice/robot/movement/waypoints/maxlinespeed/"+ str(speed))["success"]
 
 def set_waypoint_max_rotate_speed(ctx, speed):
-    """Set max rotate speed
+    """设置多路点运动时最大旋转速度
+
     Args:
-        ctx: Context
-        speed: float: Max rotate speed
+        ctx (context.Context): 登陆上下文
+        speed (float): 关节速度， 单位：度/秒
 
     Returns:
         Success: True
         Failure: False
     """
-    r = ctx.tran.put("/v2/movementservice/robot/movement/waypoints/maxrotatespeed/"+ str(speed))
-    if r[0] != 200:
-        return False
 
-    return r[1]
+    return ctx.tran.put("/v2/movementservice/robot/movement/waypoints/maxrotatespeed/"+ str(speed))["success"]
 
 def add_waypoint(ctx, target_pos):
-    """Add waypoint info
+    """添加路点信息
+
     Args:
-        ctx: Context
-        target_pos: float list: Target position
+        ctx (context.Context): 登陆上下文
+        target_pos (float list): 目标位置（关节角）
 
     Returns:
         Success: True
         Failure: False
     """
-    r = ctx.tran.post("/v2/movementservice/robot/movement/waypoints", target_pos)
-    if r[0] != 200:
-        return False
 
-    return r[1]
+    return ctx.tran.post("/v2/movementservice/robot/movement/waypoints", target_pos)["success"]
 
 def clear_waypoint(ctx):
-    """Clear waypoint info
+    """清除路点信息
+
     Args:
-        ctx: Context
+        ctx (context.Context): 登陆上下文
 
     Returns:
         Success: True
         Failure: False
     """
-    r = ctx.tran.delete("/v2/movementservice/robot/movement/waypoints/all")
-    if r[0] != 200:
-        return False
 
-    return r[1]
+    return ctx.tran.delete("/v2/movementservice/robot/movement/waypoints/all")["success"]
 
 
-def track_move(ctx):
-    """Track movement
+def track_move(ctx, target_pos, move_type, pl):
+    """同步轨迹运动,一次性运行添加的所有路点
+
     Args:
-        ctx: Context
+        ctx (context.Context): 登陆上下文
+        target_pos (float list): 目标点，与addWaypoint最后一次调用的目标点相同
+        move_type (int): 运动类型（关节、直线、旋转、圆弧）
+        pl (int): 平滑度等级，范围[0~7]
 
     Returns:
-        Success: 0
+        Success: True
         Failure: False
     """
+
+    data = {
+        "moveType": move_type,
+        "pl": pl
+    }
+
+    return ctx.tran.post("/v2/movementservice/robot/movement/rotate", data)["success"]
 
 def stop(ctx):
-    """Stop robot
+    """停止机器人运行
+
     Args:
-        ctx: Context
+        ctx (context.Context): 登陆上下文
 
     Returns:
-        Success: 0
+        Success: True
         Failure: False
     """
-    r = ctx.tran.post("/v2/movementservice/robot/movement/stop")
-    if r[0] != 200:
-        return False
 
-    return r[1]
+    return ctx.tran.post("/v2/movementservice/robot/movement/stop")["success"]
